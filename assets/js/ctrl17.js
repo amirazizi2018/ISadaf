c3e852b7adf4d1e44136ce4d8de022cb3b694779
A20Group <A20Group@yahoo.com>
2018-01-11 19:34:01 +0330

    Edit Add Kedmat V1.4.7

--- assets/js/ctrl17.js
+++ assets/js/ctrl17.js
@@ -876,28 +876,28 @@ app.controller('spdashboardpage', function($compile, $sce, $scope, $window, $htt
 
    */
 
-  // $scope.CategoryExam = ["رزومه", "روانشناختی", "مهارتی", "شایستگی"];
-  // $scope.getvahedzaman = [
-  //   {
-  //     "faname": "دقیقه",
-  //     "enname": "min",
-  //   },
-  //   {
-  //     "faname": "ساعت",
-  //     "enname": "hur",
-  //   },
-  // ];
-  //
-  // $scope.dastekhedmatselected = "رزومه";
-  //
-  //
-  // $http.get(mustafasite + '/exam').then(function(response) {
-  //   $scope.getexamnameid = response.data.exams;
-  // });
-  //
-  // $http.get(mustafasite + '/suitability/package').then(function(response) {
-  //   $scope.getsuitabilitynameid = response.data.packages;
-  // });
+  $scope.CategoryExam = ["رزومه", "روانشناختی", "مهارتی", "شایستگی"];
+  $scope.getvahedzaman = [
+    {
+      "faname": "دقیقه",
+      "enname": "min",
+    },
+    {
+      "faname": "ساعت",
+      "enname": "hur",
+    },
+  ];
+
+  $scope.dastekhedmatselected = "رزومه";
+
+
+  $http.get(mustafasite + '/exam').then(function(response) {
+    $scope.getexamnameid = response.data.exams;
+  });
+
+  $http.get(mustafasite + '/suitability/package').then(function(response) {
+    $scope.getsuitabilitynameid = response.data.packages;
+  });
 
 
   // $scope.chengedastekhedmat = function() {
